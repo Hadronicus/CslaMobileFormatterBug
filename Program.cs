873e460c4bc19432c078378759be55252533cb53
Hadronicus <40719377+Hadronicus@users.noreply.github.com>
2019-03-11 16:00:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Csla;
+using Csla.Configuration;
+using System;
+
+namespace CslaMobileFormatterBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationManager.AppSettings.Set("CslaSerializationFormatter", "MobileFormatter");
+
+            var sbo = DataPortal.Create<SimpleBusinessObject>();
+
+            sbo.BeginEdit();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleBusinessObject.cs
@@ -0,0 +1,32 @@
+﻿using Csla;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CslaMobileFormatterBug
+{
+    [Serializable]
+    public class SimpleBusinessObject : BusinessBase<SimpleBusinessObject>
+    {
+        private static readonly PropertyInfo<int> _id = RegisterProperty<int>(p => p.Id);
+
+        public int Id
+        {
+            get => GetProperty(_id);
+            set => SetProperty(_id, value);
+        }
+
+        private static readonly PropertyInfo<string> _name = RegisterProperty<string>(p => p.Name);
+
+        public string Name
+        {
+            get => GetProperty(_name);
+            set => SetProperty(_name, value);
+        }
+
+        [RunLocal]
+        protected override void DataPortal_Create() => base.DataPortal_Create();
+    }
+}
